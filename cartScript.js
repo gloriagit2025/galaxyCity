903dc18b35695bb2b8dd38db3ffdb1ae7bef7dab
Gloria <glorychun@gmail.com>
2025-06-03 17:51:04 +0800

    +database格式

---
+++ cartScript.js
@@ -0,0 +1,7 @@
+fetch("/database.json")
+  .then(response => response.json()) // 正確解析 JSON 數據
+  .then(userData => {
+    let newDiv = document.createElement("div");
+    newDiv.textContent = JSON.stringify(userData); // 確保顯示為字串
+    document.body.appendChild(newDiv);
+  })
