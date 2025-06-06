233c1ecca41ecf1886f5c82977e8cc5f4f86e908
Biu <gmail>
2025-06-06 10:56:56 +0800

    +產品介面基礎

--- productListScript.js
+++ productListScript.js
@@ -1,10 +1,12 @@
-fetch("/database.json")
-  .then((response) => response.json()) // 正確解析 JSON 數據
-  .then((productData) => {
-    productData.forEach((data) => {
-      let newDiv = document.createElement("div");
-      newDiv.innerHTML="";
-
-      document.getElementById("productPanel").appendChild(newDiv);
+let productPanel = document.getElementById("productPanel");//獲得產品介面元素
+fetch("/database.json")//請求讀取該檔案
+  .then((response) => response.json())//獲取的檔案轉為json物件
+  .then((productData) => {//獲得產品資料庫參數
+    productPanel.innerHTML = "";//重置產品介面元素為空
+    productData.forEach((data) => {//遍歷每個產品
+      let newDiv = document.createElement("div");//創建div元素
+      newDiv.className = "row"//屬性設為bootstrap row
+      newDiv.innerHTML = `<p class="col-3">${data.name}</p><img class="col-3" src="${data.imgUrl}" alt="${data.name}">`;
+      productPanel.appendChild(newDiv);
     });
   });
