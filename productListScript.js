bbb6509cfe2f9b1979f0dc4d4f01ef38559746df
Biu <gmail>
2025-06-06 15:40:12 +0800

    +產品列表購物按鈕

--- productListScript.js
+++ productListScript.js
@@ -1,12 +1,20 @@
 let productPanel = document.getElementById("productPanel");//獲得產品介面元素
-fetch("/database.json")//請求讀取該檔案
+fetch("database.json")//請求讀取該檔案
   .then((response) => response.json())//獲取的檔案轉為json物件
   .then((productData) => {//獲得產品資料庫參數
     productPanel.innerHTML = "";//重置產品介面元素為空
     productData.forEach((data) => {//遍歷每個產品
       let newDiv = document.createElement("div");//創建div元素
-      newDiv.className = "row"//屬性設為bootstrap row
-      newDiv.innerHTML = `<p class="col-3">${data.name}</p><img class="col-3" src="${data.imgUrl}" alt="${data.name}">`;
+      newDiv.classList.add("row")//屬性添加bootstrap row
+      newDiv.innerHTML =
+      `<img class="col-3" src="${data.imgUrl}" alt="${data.name}">
+       <div class="col-3">
+        <div>${data.name}</div>
+        <div>HK$${data.price}</div>
+        <button onclick="addToCart('${data.name}')">加入購物車</button>
+       </div>`;
       productPanel.appendChild(newDiv);
     });
   });
+  function addToCart(productName){
+  }
