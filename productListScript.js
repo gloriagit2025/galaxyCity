8931e1b56c6eed966b654679c12c4e94eb0d552e
codeman514 <ycl2004191a@gmail.com>
2025-06-11 00:30:41 +0800

    +產品列表分類功能

--- productListScript.js
+++ productListScript.js
@@ -1,24 +1,32 @@
-let cartProductList = [];//重置購物車列表
+let cartProductList = JSON.parse(localStorage.getItem("cartProductList"));//從localStorage讀取購物車列表
+let categoryList = document.querySelectorAll("#categoryList li"); //獲取所有分類按鈕
+let showCategory = "全部商品"; //默認顯示分類為全部商品
 let productPanel = document.getElementById("productPanel"); //獲得產品介面元素
-fetch("database.json") //請求讀取該檔案
-  .then((response) => response.json()) //獲取的檔案轉為json物件
-  .then((productData) => {
-    //獲得產品資料庫參數
-    productPanel.innerHTML = ""; //重置產品介面元素為空
-    productData.forEach((data) => {
+updateCartPanel();
+function updateCartPanel() {
+  fetch("database.json") //請求讀取該檔案
+    .then((response) => response.json()) //獲取的檔案轉為json物件
+    .then((productData) => {
+      //獲得產品資料庫參數
+      productPanel.innerHTML = ""; //重置產品介面元素為空
       //遍歷每個產品
-      let newDiv = document.createElement("div"); //創建div元素
-      newDiv.classList.add("row"); //屬性添加bootstrap row
-      newDiv.innerHTML =
-        `<img class="col-3" src="${data.imgUrl}" alt="${data.name}">
+      productData.forEach((data) => {
+        //如果現時顯示分類為全部商品或產品分類為現時顯示分類
+        if (showCategory === "全部商品" || data.category === showCategory) {
+          let newDiv = document.createElement("div"); //創建div元素
+          newDiv.classList.add("row"); //屬性添加bootstrap row
+          newDiv.innerHTML =
+            `<img class="col-3" src="${data.imgUrl}" alt="${data.name}">
        <div class="col-3">
         <div>${data.name}</div>
         <div>HK$${data.price}</div>
         <button onclick='addToCart(${JSON.stringify(data)})'>加入購物車</button>
        </div>`;
-      productPanel.appendChild(newDiv);
+          productPanel.appendChild(newDiv);
+        }
+      });
     });
-  });
+}
 function addToCart(product) {//加入購物車
   //查找產品名字在購物車的序列
   let indexInCartProductList = cartProductList.findIndex(
@@ -39,3 +47,15 @@ function addToCart(product) {//加入購物車
   }
   localStorage.setItem("cartProductList", JSON.stringify(cartProductList)); //將購物車列表存入localStorage
 }
+//為分類列表每個分類按鈕添加點擊事件
+categoryList.forEach((category) => {
+  category.addEventListener("click", () => {
+    //遍歷每個分類按鈕去移除所有分類按鈕的類別
+    categoryList.forEach((category) => {
+      category.classList.remove("active"); //移除該按鈕的active類別
+    });
+    category.classList.add("active"); //使點擊的分類按鈕高亮active類別
+    showCategory = category.textContent; //現時分類為該分類按鈕文字內容
+    updateCartPanel(); //更新產品介面
+  })
+});
