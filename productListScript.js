71de927e17a443e28a75e5c5b09163e747b23c1e
Biu <gmail>
2025-06-12 09:20:50 +0800

    +修正初始化購物車

--- productListScript.js
+++ productListScript.js
@@ -1,19 +1,25 @@
-let cartProductList = JSON.parse(localStorage.getItem("cartProductList"));//從localStorage讀取購物車列表
+let cartProductList = JSON.parse(localStorage.getItem("cartProductList")); //從localStorage讀取購物車列表
+//如未有購物車列表則初始化
+if (cartProductList == null) {
+  cartProductList = [];
+}
 let categoryList = document.querySelectorAll("#categoryList li"); //獲取所有分類按鈕
 let showCategory = "全部商品"; //默認顯示分類為全部商品
 let productPanel = document.getElementById("productPanel"); //獲得產品介面元素
 let confirmTips = document.querySelector(".confirmTips"); //獲得購物車確認提示框
 let confirmButton = document.getElementById("closeConfirmBtn"); //獲得確認按鈕
 updateCartPanel();
-function updateCartPanel() {//更新購物車介面
+function updateCartPanel() {
+  //更新購物車介面
   localStorage.setItem("cartProductList", JSON.stringify(cartProductList)); //將購物車列表存入localStorage
   let cartQuantity = 0; //重置購物車數量計數器
   //遍歷每個購物車產品
   cartProductList.forEach((data) => {
     cartQuantity += data.quantity; //累加總數量
   });
-  localStorage.setItem("cartQuantity",cartQuantity)//將購物車數量存入localStorage
-  document.querySelector(".cart-count").textContent = localStorage.getItem("cartQuantity"); //更新購物車數量顯示
+  localStorage.setItem("cartQuantity", cartQuantity); //將購物車數量存入localStorage
+  document.querySelector(".cart-count").textContent =
+    localStorage.getItem("cartQuantity"); //更新購物車數量顯示
   fetch("database.json") //請求讀取該檔案
     .then((response) => response.json()) //獲取的檔案轉為json物件
     .then((productData) => {
@@ -25,8 +31,9 @@ function updateCartPanel() {//更新購物車介面
         if (showCategory === "全部商品" || data.category === showCategory) {
           let newDiv = document.createElement("div"); //創建div元素
           newDiv.classList.add("row"); //屬性添加bootstrap row
-          newDiv.innerHTML =
-            `<img class="col-3" src="${data.imgUrl}" alt="${data.name}">
+          newDiv.innerHTML = `<img class="col-3" src="${data.imgUrl}" alt="${
+            data.name
+          }">
        <div class="col-3">
         <div>${data.name}</div>
         <div>HK$${data.price}</div>
@@ -35,10 +42,10 @@ function updateCartPanel() {//更新購物車介面
           productPanel.appendChild(newDiv);
         }
       });
-    }
-    );
+    });
 }
-function addToCart(product) {//加入購物車
+function addToCart(product) {
+  //加入購物車
   //查找產品名字在購物車的序列
   let indexInCartProductList = cartProductList.findIndex(
     (value) => value.name === product.name
@@ -50,11 +57,10 @@ function addToCart(product) {//加入購物車
       name: product.name,
       imgUrl: product.imgUrl,
       price: product.price,
-      quantity: 1
+      quantity: 1,
     });
-  }
-  else {
-    cartProductList[indexInCartProductList].quantity++;//購物車列表內已有產品名字的數量增加
+  } else {
+    cartProductList[indexInCartProductList].quantity++; //購物車列表內已有產品名字的數量增加
   }
   updateCartPanel(); //更新購物車介面
   confirmTips.style.display = "block"; //顯示購物車確認提示框
@@ -69,9 +75,9 @@ categoryList.forEach((category) => {
     category.classList.add("active"); //使點擊的分類按鈕高亮active類別
     showCategory = category.textContent; //現時分類為該分類按鈕文字內容
     updateCartPanel(); //更新購物車介面
-  })
+  });
 });
 //為購物車關閉按鈕添加點擊隱藏提示框事件
 confirmButton.addEventListener("click", () => {
   confirmTips.style.display = "none"; //隱藏購物車確認提示框
-})
+});
