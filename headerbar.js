b922832adeb812f2821eb9a146974f4593be1165
Lok <loktseweb@gmail.com>
2025-09-21 23:44:58 +0800

    add logout

--- headerbar.js
+++ headerbar.js
@@ -5,8 +5,8 @@ if(localStorage.getItem('user')){
   document.getElementById('loginStatusCol').innerHTML='<a class="login-status" href="profile.html" >會員中心</a> \
   | <a class="login-status" onclick="logout()" >登出</a>';
 }else{
-    document.getElementById('loginStatus').innerHTML='<a  href="login.html">登入</a> | <a href="register.html">註冊</a>';
-    document.getElementById('loginStatusCol').innerHTML='<a  href="login.html">登入</a> | <a href="register.html">註冊</a>';
+    document.getElementById('loginStatus').innerHTML='<a href="login.html">登入</a> | <a href="register.html">註冊</a>';
+    document.getElementById('loginStatusCol').innerHTML='<a href="login.html">登入</a> | <a href="register.html">註冊</a>';
 }
 
 
